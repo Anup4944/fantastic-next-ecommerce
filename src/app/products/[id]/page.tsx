60b744cdedf04f3767110aca15b36001568c6ea1
Anup <anuppoudel60@gmail.com>
2023-07-21 10:25:49 +1000

    add to cart done

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,6 +4,8 @@ import { Metadata } from "next"
 import Image from "next/image"
 import { notFound } from "next/navigation"
 import {cache} from "react"
+import AddToCart from "./AddToCart"
+import increaseProdQty from "./actions"
 
 interface ProductPageProps{
   params:{
@@ -49,6 +51,7 @@ return (
       <h1 className="text-5xl font-bold">{product.name}</h1>
       <PriceTag price={product.price} className="mt-4" />
       <p className="py-6">{product.description}</p>
+      <AddToCart productId={product.id} increaseProdQty={increaseProdQty}/>
     </div>
   </div>
 
