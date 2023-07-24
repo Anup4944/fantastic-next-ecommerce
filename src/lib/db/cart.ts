a20902135e110fbfa4c49f4cc5e64574942d50b6
Anup <anuppoudel60@gmail.com>
2023-07-24 15:57:34 +1000

    cart page done

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -10,6 +10,10 @@ export type CartWithProducts = Prisma.CartGetPayload<{
     include:{items:{include:{product:true}}}
 }>
 
+export type CartItemWithProduct = Prisma.CartItemsGetPayload<{
+    include: {product:true}
+}>
+
 export type ShoppingCart = CartWithProducts & { 
     size :number,
     subTotal: number,
