28b4c5989671dd088ccc11e59f43d44fcb390326
Anup <anuppoudel60@gmail.com>
2023-07-26 09:38:58 +1000

    auth added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "plus.unsplash.com" }],
+    remotePatterns: [{ hostname: "plus.unsplash.com" },{hostname: "lh3.googleusercontent.com"}],
   },
   
   experimental: {
--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -2,7 +2,11 @@ import { redirect } from "next/navigation"
 import Link from "next/link"
 import { getCart } from "@/lib/db/cart"
 import ShoppingCartBtn from "./ShoppingCartBtn"
-// import Image from "next/image"
+import Image from "next/image"
+import logo from "@/assets/logo.png";
+import UserMenuBtn from "./UserMenuBtn"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
 
 async function searchProducts(formData:FormData){
     "use server"
@@ -15,16 +19,16 @@ async function searchProducts(formData:FormData){
 }
 
 export default async function Navbar(){
-
+    const session = await getServerSession(authOptions)
     const cart  = await getCart()
     return (
         <div className="bg-base-100">
             <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2 ">
                 <div className="flex-1 ">
                     <Link href="/" className="btn btn-ghost text-xl normal-case">
-                        {/* <Image src={"https://images.unsplash.com/photo-1567446537708-ac4aa75c9c28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"} alt="Logo"
+                         <Image src={logo} alt="Logo"
                         height={40} width={40}
-                        /> */} Nextzone
+                        /> Nextzone
                     </Link>
 
                 </div>
@@ -36,6 +40,7 @@ export default async function Navbar(){
                         </div>
                     </form>
                     <ShoppingCartBtn cart={cart}/>
+                    <UserMenuBtn session={session} />
 
                 </div>
 
